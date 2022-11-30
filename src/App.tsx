4c46125e7be178efa51a3beb7c3acb359b890760
Ratan Mihyar <r.mihyar@alto-solutions.com>
2022-11-30 18:41:47 +0200

    'init'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useMemo } from 'react';
 import './App.css';
+import ReactDOM from "react-dom";
+import DataTable from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import SortIcon from "@material-ui/icons/ArrowDownward";
+import Data from "./db.json";
 
+interface Data { 
+  id: number;
+
+}
 function App() {
+  const columns = useMemo(
+    () => [
+        { Header: "ID", accessor: "id" },
+        { Header: "First name", accessor: "first_name" }   
+    ],
+    []
+   );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <DataTable
+          title="SunShine"
+          columns={columns}
+          data={Data}
+          sortIcon={<SortIcon />}
+          pagination
+          selectableRows
+        />
+      </Card>
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;

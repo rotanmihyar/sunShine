67165697417fa2da0b218fad6427afb969b72d97
Ratan Mihyar <r.mihyar@alto-solutions.com>
2022-11-30 19:03:41 +0200

    'init'

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useMemo } from 'react';
+import React from 'react';
 import './App.css';
 import ReactDOM from "react-dom";
 import DataTable from "react-data-table-component";
 import Card from "@material-ui/core/Card";
 import SortIcon from "@material-ui/icons/ArrowDownward";
 import Data from "./db.json";
+const columns = [
+  {
+    First: "Id ",
+    selector: "id",
+    sortable: true,
+  },
+  {
+    name: "First Name",
+    selector: "firstName",
+    sortable: false
+  }
+];
 
-interface Data { 
-  id: number;
-
-}
 function App() {
-  const columns = useMemo(
-    () => [
-        { Header: "ID", accessor: "id" },
-        { Header: "First name", accessor: "first_name" }   
-    ],
-    []
-   );
   return (
     <div className="App">
       <Card>
@@ -25,6 +26,7 @@ function App() {
           title="SunShine"
           columns={columns}
           data={Data}
+          defaultSortField="id"
           sortIcon={<SortIcon />}
           pagination
           selectableRows
